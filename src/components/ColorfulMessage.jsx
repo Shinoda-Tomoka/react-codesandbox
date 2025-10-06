31016c3eb4e0d54477220b2176054021bbc817bf
tomoka <Tomoka.Shinoda2@jp.ricoh.com>
2025-10-06 05:16:16 +0000

    53-54 TODOアプリ作成準備

--- src/components/ColorfulMessage.jsx
+++
@@ -1,9 +0,0 @@
-export const ColorfulMessage = ({ color, children }) => {
-  console.log("---color---");
-  const contentStyleA = {
-    color,
-    fontSize: "18px",
-  };
-
-  return <p style={contentStyleA}>{children}</p>;
-};
